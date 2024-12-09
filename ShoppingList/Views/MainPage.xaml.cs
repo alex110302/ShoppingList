5cb37043e0f1a3c14093d32bb4e85f0ddb379f97
alex110302 <hansenaalexander@gmail.com>
2024-12-08 23:02:17 -0800

    Added list page stuff

--- ShoppingList/Views/MainPage.xaml.cs
+++ ShoppingList/Views/MainPage.xaml.cs
@@ -18,6 +18,11 @@ public partial class MainPage : ContentPage
         Title = "Shopping List Pro";
         Loaded += MainPage_Loaded;
         _loginPage.Unloaded += LoginPage_Unloaded;
+        lstData.Refreshing += delegate
+        {
+            LoadData();
+            lstData.IsRefreshing = false;
+        };
     }
 
     private void LoginPage_Unloaded(object sender, EventArgs e)
@@ -33,7 +38,7 @@ public partial class MainPage : ContentPage
     public void OnAppStart()
     {
         if (string.IsNullOrEmpty(App.SessionKey)) Navigation.PushModalAsync(new NavigationPage(_loginPage));
-        else txtInput.Text = App.SessionKey;
+        else LoadData();
     }
 
     private async void BtnLogout_OnClicked(object sender, EventArgs e)
@@ -45,5 +50,62 @@ public partial class MainPage : ContentPage
         
         App.SessionKey = "";
         OnAppStart();
+    }
+
+    private async void BtnAdd_OnClicked(object sender, EventArgs e)
+    {
+        string data = JsonConvert.SerializeObject(new UserData( null, txtInput.Text, App.SessionKey));
+        HttpClient client = new HttpClient();
+        await client.PostAsync(new Uri("https://joewetzel.com/fvtc/account/data"),
+            new StringContent(data, Encoding.UTF8, "application/json"));
+
+        txtInput.Text = string.Empty;
+        LoadData();
+    }
+
+    public async void LoadData()
+    {
+        var response = await new HttpClient().GetAsync(new Uri($"https://joewetzel.com/fvtc/account/data/{App.SessionKey}"));
+        var wsJson = response.Content.ReadAsStringAsync().Result;
+        var userDataObject = JsonConvert.DeserializeObject<UserDataCollection>(wsJson);
+
+        lstData.ItemsSource = userDataObject.UserDataItems;
+    }
+
+    private async void MenuItem_OnClicked(object sender, EventArgs e)
+    {
+        var dataID = ((MenuItem)sender).CommandParameter.ToString();
+        var data = JsonConvert.SerializeObject(new UserData(dataID, null, App.SessionKey));
+
+        var client = new HttpClient();
+        
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Delete,
+            RequestUri = new Uri("https://joewetzel.com/fvtc/account/data"),
+            Content = new StringContent(data, Encoding.UTF8, "application/json")
+        };
+
+        await client.SendAsync(request);
+        
+        LoadData();
+    }
+
+    private async void BtnClearList_OnClicked(object sender, EventArgs e)
+    {
+        var data = JsonConvert.SerializeObject(new UserData(null, null, App.SessionKey));
+        var test = App.SessionKey;
+        var client = new HttpClient();
+        
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Delete,
+            RequestUri = new Uri("https://joewetzel.com/fvtc/account/data"),
+            Content = new StringContent(data, Encoding.UTF8, "application/json")
+        };
+
+        await client.SendAsync(request);
+        
+        LoadData();
     }
 }
